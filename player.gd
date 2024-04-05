c6f6a9b4974894e1a7716c08c00b4bf668962754
John Christensen <johnedchristensen@gmail.com>
2024-04-05 13:46:33 -0500

    Add improved player movement

--- player.gd
+++ player.gd
@@ -2,23 +2,37 @@ class_name Player
 
 extends CharacterBody2D
 
-const SPEED = 300.0
-const SPRINT_FACTOR = 3
+@export var TOP_SPEED  = 600.0
+@export var MIN_SPEED  = 300.0
+
+var SECONDS_TO_TOP_SPEED = 100
+var ACCELERATION = (TOP_SPEED-MIN_SPEED)/SECONDS_TO_TOP_SPEED
+
+@export var ROTATION_SPEED = 4*PI
+var SPRINT_FACTOR = 3
 
 @onready var attack_box_area = $AttackBoxArea
 
-func _physics_process(delta: float) -> void:
-	var direction = Vector2(0,0)
-	if Input.is_action_pressed("Move Right"):
-		direction += Vector2(1,0)
-	if Input.is_action_pressed("Move Left"):
-		direction += Vector2(-1,0)
-	if Input.is_action_pressed("Move Up"):
-		direction += Vector2(0,-1)
-	if Input.is_action_pressed("Move Down"):
-		direction += Vector2(0,1)
+func update_rotation_lerp(start,end,amount) -> float:
+		return lerp_angle(start,end,amount)
+
 	
-	velocity = direction * SPEED 
+
+func _physics_process(delta: float) -> void:
+	#input
+	var input_direction = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
+	velocity = input_direction * clamp(velocity.length()+(ACCELERATION),MIN_SPEED,TOP_SPEED)
+
+	var target_angle = input_direction.angle()
+	rotation = update_rotation_lerp(rotation,target_angle,ROTATION_SPEED * delta)
+	
 	if Input.is_action_pressed("Sprint"):
 		velocity *= SPRINT_FACTOR
-	move_and_slide()
+	#process
+
+	#$AnimatedSprite2D.flip_v = velocity.x < 0
+	$AnimatedSprite2D.flip_v = abs(rotation) > PI/2
+	# using move_and_collide
+	var collision = move_and_collide(velocity*delta)
+	if collision:
+		velocity = velocity.slide(collision.get_normal())
